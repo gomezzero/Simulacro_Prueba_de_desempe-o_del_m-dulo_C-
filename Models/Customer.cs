73a2008e218fed30879ffe66fedbaf937f54135a
gomez-zero <gomezjuanzero@gmail.com>
2024-08-02 06:55:30 -0500

    feat: adding Models folder with their respective empty classes

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_Prueba_de_desempe_o_del_m_dulo_C_.Models
+{
+    public class Customer
+    {
+        
+    }
+}
